0638bee9f518356f20ef02457cf28dcd979430f7
AcunaTomas <xxtomyacuxx@gmail.com>
2021-06-27 00:54:57 -0300

    VER 9.0

--- code/Init.js
+++ code/Init.js
@@ -1,7 +1,12 @@
 var config = {
     type: Phaser.WEBGL,
-    width: 800,
-    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 800,
+        height: 600,
+    },
+
     physics: {
         default: "arcade",
         arcade: {
@@ -9,7 +14,7 @@ var config = {
             debug: false
         }
     },
-    scene: [loader, Lvl1]
+    scene: [loader,menu, Lvl1, Lvl2]
     
 };
 
@@ -24,6 +29,8 @@ var birds;
 var map;
 var tileset;
 var lvljuan;
+var lvldos;
+
 
 //Movement keys
 var cursors;
@@ -54,7 +61,10 @@ var time = 0;
 var limit;
 //Functionality
 var gameover = false
-
+var lvlcomplete = false
+var paths
+var lives = 3
+var deathcause = 0
 
 
 var game = new Phaser.Game(config);
--- code/Lvl2.js
+++ code/Lvl2.js
@@ -1,30 +1,30 @@
-class Lvl1 extends Phaser.Scene
+class Lvl2 extends Phaser.Scene
 {
     constructor()
     {
-        super('Lvl1')
+        super('Lvl2')
     }
 
     create()
     {
         //Creation - Camera, Physics Groups, Solid Objects, Controls
         coolcam= this.cameras.main;
-        this.cameras.main.setBounds(0,0,5600,1600);
-        background = this.add.image(1000,800, 'background');
-        background = this.add.image(3000,800, 'background');
-        background = this.add.image(5000,800, 'background');
+        this.cameras.main.setBounds(0,0,4850,2875);
+        background = this.add.image(2500,1600, 'background2')
 
-        map = this.make.tilemap({key: 'map1'});
-        tileset = map.addTilesetImage('Tilemap1', 'tiles');
-        lvljuan = map.createLayer('1', tileset);
-        lvljuan.setCollisionBetween(0,2);
+        map = this.make.tilemap({key: 'map2'});
+        tileset = map.addTilesetImage('Tilemap2', 'tile2');
+        lvldos = map.createLayer('Tile Layer 1', tileset);
+        lvldos.setCollisionBetween(0,1);
+        lvldos.setCollisionBetween(1,12);
 
         rings = this.physics.add.staticGroup();
         fueltank = this.physics.add.group({});
         birds = this.physics.add.group({})
 
         var sp = map.findObject("obj1", obj => obj.name === "sp");
-        var aea = map.createFromObjects("obj1", name = "Tank" ,{key : 'fuel'})
+        var jp = map.findObject('obj1', obj => obj.name === "jp")
+        var aea = map.createFromObjects("tank", name = "Tank" ,{key : 'fuel'})
         fueltank.addMultiple(aea, true)
         fueltank.children.iterate(function (child)
         {
@@ -49,8 +49,6 @@ class Lvl1 extends Phaser.Scene
 
         }
         )
-        
-        deathcause = 0
 
         cursors = this.input.keyboard.createCursorKeys();
         keya = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
@@ -58,11 +56,11 @@ class Lvl1 extends Phaser.Scene
         keyd = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         keyw = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
 
-
+        deathcause = 0
         hoopcombo = 0
         score = 0
         pwup = false
-        limit = this.time.delayedCall(120000, this.onEvent, [], this);
+        limit = this.time.delayedCall(120000, this.onEvent, [], this);;
         this.time.addEvent(limit);
 
         //HUD
@@ -71,7 +69,7 @@ class Lvl1 extends Phaser.Scene
         combotext.scrollFactorY = 0
 
         jugde = this.physics.add.staticGroup();
-        jugde.create(5575, 1500, 'judge');
+        jugde.create(jp.x, jp.y, 'judge');
         jscore = this.add.text(300, 400, '', { fontSize: '72px', fill: '#000' });
         jscore.setScrollFactor(0);
         
@@ -79,19 +77,23 @@ class Lvl1 extends Phaser.Scene
         player = this.physics.add.sprite(sp.x,sp.y,'marselo');
 
         //Colliders
-        this.physics.add.collider(player, lvljuan);
-        this.physics.add.collider(player, platforms);
-        this.physics.add.collider(fueltank, lvljuan);
-        this.physics.add.collider(birds, lvljuan);
+        this.physics.add.collider(player, lvldos);
+        this.physics.add.collider(fueltank, lvldos);
+        this.physics.add.collider(birds, lvldos);
         this.physics.add.overlap(player, fueltank, this.boost, null, this);
         this.physics.add.overlap(player, jugde, this.endlevel, null, this);
         this.physics.add.overlap(player, rings, this.ringcombo, null, this);
         this.physics.add.overlap(player, birds, this.death, null, this);
 
+
     }
 
     update(delta)
     {
+        if (player.y > 2900)
+        {
+            this.death()
+        }
         if (lvlcomplete)
         {
             if (deathcause == 0)
@@ -129,8 +131,6 @@ class Lvl1 extends Phaser.Scene
                         gameover = false
                         lvlcomplete = false
                         time = delta
-                        comboval = 0
-                        deathcause = 0
                     }
 
                     else
@@ -140,13 +140,13 @@ class Lvl1 extends Phaser.Scene
                         lvlcomplete = false
                         time = delta
                         lives += -1
-                        deathcause = 0
                     }
 
                 }
                 else
                 {
-                    this.scene.start('Lvl2')
+                    this.scene.start('menu')
+                    gameover = false
                     lvlcomplete = false
                 }
                 
@@ -154,77 +154,95 @@ class Lvl1 extends Phaser.Scene
         }
         else
         {
-            coolcam.centerOn(player.x,player.y);
+        coolcam.centerOn(player.x,player.y);
         
-            var rt = delta - time
-    
-            var lastring = rt - hoopcombo
-    
-                if (comboval == 1 && firstpickup == true)
-                {
-                    hoopcombo = rt
-                    firstpickup = false
-                }
-                else if (lastring <= 10000 && lastring < comboval)
-                {
-                hoopcombo = rt;
-                }
-                else if (lastring > 10000 || comboval >= 10)
-                {
-                score += comboval*200;
-                comboval = 0;
-                }
-    
-            var l = limit.getProgress();
-            combotext.setText('Time:' + l.toString().substr(2,4) + ' Combo:' + comboval.toString() + ' Score:' + score.toString() + " Lives: " + lives)
-            
-    
-            if (player.body.blocked.down)
+        var rt = delta - time
+
+        var lastring = rt - hoopcombo
+
+            if (comboval == 1 && firstpickup == true)
             {
-                if (pwup == true)
-                {
-                    launch = 2
-                }
-                else
-                {
-                    launch = 1
-                }
+                hoopcombo = rt
+                firstpickup = false
             }
-    
-    
-            if (keya.isDown)
+            else if (lastring <= 10000 && lastring < comboval)
             {
-                player.setVelocityX(-200)
-                player.anims.play('right', true)
-                player.flipX = true
+            hoopcombo = rt;
             }
-            else if (keyd.isDown)
+            else if (lastring > 10000 || comboval >= 10)
             {
-                player.setVelocityX(200)
-                player.anims.play('right', true)
-                player.flipX = false
+            score += comboval*200;
+            comboval = 0;
+            }
+
+        var l = limit.getProgress();
+        combotext.setText('Time:' + l.toString().substr(2,4) + ' Combo:' + comboval.toString() + ' Score:' + score.toString() + ' Lives: ' + lives)
+        
+
+        if (player.body.blocked.down)
+        {
+            if (pwup == true)
+            {
+                launch = 2
             }
             else
             {
-                player.setVelocityX(0);
-                player.anims.play('idle', true)
+                launch = 1
             }
-    
-            if (cursors.space.isDown && player.body.blocked.down)
-            {
-                player.setVelocityY(-300)
-            }
-    
-            if (Phaser.Input.Keyboard.JustDown(cursors.space) && launch > 0 && player.body.blocked.down == false)
-            {
-                player.setVelocityY(-400)
-                launch += -1
-            }
-    
-            
         }
 
 
+        if (keya.isDown)
+        {
+            player.setVelocityX(-200)
+            player.anims.play('right', true)
+            player.flipX = true
+        }
+        else if (keyd.isDown)
+        {
+            player.setVelocityX(200)
+            player.anims.play('right', true)
+            player.flipX = false
+        }
+        else
+        {
+            player.setVelocityX(0);
+            player.anims.play('idle', true)
+        }
+
+        if (cursors.space.isDown && player.body.blocked.down)
+        {
+            player.setVelocityY(-300)
+        }
+
+        if (Phaser.Input.Keyboard.JustDown(cursors.space) && launch > 0 && player.body.blocked.down == false)
+        {
+            player.setVelocityY(-400)
+            launch += -1
+        }
+
+        birds.children.iterate(function (child)
+        {
+            if (player.x - child.x < 30 && player.y - child.y < 60)
+            {
+                child.body.setVelocityX(-75)
+                child.body.setAccelerationX(-5)
+            }
+            else if (child.x - player.x < -30 && player.y - child.y > 60)
+            {
+                child.body.setVelocityX(75)
+                child.body.setAccelerationX(5)
+            }
+            else
+            {
+                child.body.setVelocityX(0)
+                child.body.setAccelerationX(0)
+            }
+        }
+        )
+
+        }
+        
     }
 
     boost(player, fueltank)
@@ -253,16 +271,32 @@ class Lvl1 extends Phaser.Scene
     {
         lvlcomplete = true
     }
-    
     onEvent()
     {
-        lvlcomplete = true
-        deathcause = 1
+        lives += -1
+        if (lives < 1)
+        {
+            lvlcomplete = true
+        }
+        else
+        {
+            this.scene.restart()
+            score = 0
+        }
+        
     }
 
     death()
     {
-        lvlcomplete = true
-        deathcause = 1
+        lives += -1
+        if (lives < 1)
+        {
+            lvlcomplete = true
+            score = 0
+        }
+        else
+        {
+            this.scene.restart()
+        }
     }
 }
